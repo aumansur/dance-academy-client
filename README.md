be9d26757044c9dcbbc7c4d1bdf3af134e974501
aumansur <ahammadullah2001@gmail.com>
2023-06-16 03:10:25 +0600

    5 bullet ponit redme file add

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 
 ## DANCE ACADEMY 
 * first of all install the project with react router and tailwind css 
+* main page on the website Home page instructors page , classes page , dashboard, login , sign up page etc. 
 * second then home page layout setup for the project
+* Use Frontend technologies HTML, CSS, JavaScript, React, Tailwind CSS, react router, react more package install , axios , react query, 
+* Backend technologies Express JS , Node JS , MongoDB 
+# Live site link
+https://dance-academy-19d22.web.app/
--- src/components/shared_pages/classes/ClassesCard.jsx
+++ src/components/shared_pages/classes/ClassesCard.jsx
@@ -5,7 +5,7 @@ const ClassesCard = ({ item }) => {
 
       <div className="card-body relative">
         <figure> <img className="rounded-lg w-96 h-96" src={image} alt="" /></figure>
-        <h2 className="card-title text-center rounded-tl-lg rounded-br-[400px] rounded-tr-[100px] bg-[#E5AF4C] font-semibold px-4 py-1 w-40 shadow-2xl shadow-white absolute bottom-10 right-0">  {name}</h2>
+        <h2 className="card-title  text-center rounded-tl-lg rounded-br-[400px] rounded-tr-[100px] bg-[#E5AF4C] font-semibold px-4 py-1 w-44 shadow-2xl shadow-white absolute bottom-10 right-0">  {name}</h2>
 
       </div>
     </div>
